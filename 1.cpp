6e3f58dbf2b51593c32aebdc80fa3e932aaad0bb
ippapi <coricutiphong@gmail.com>
2023-03-10 11:54:55 +0700

    ko bt

--- 1.cpp
+++ 1.cpp
@@ -2,16 +2,7 @@
 #include <iostream>
 using namespace std;
 
-void inputArray(int *&a, int &n){
-
-    cin >> n;
-
-    a = new int[1000000];
-    
-    for(int i = 0; i < n; i++){
-        cin >> a[i];
-    }
-
+lmao
 }
 
 
